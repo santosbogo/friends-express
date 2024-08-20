44cffd1e270741742f5440953621dde048e8b8b1
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-08-20 16:09:50 -0300

    When create new friend now you need address

--- src/modules/friends/repository/friends.repository.impl.ts
+++ src/modules/friends/repository/friends.repository.impl.ts
@@ -6,22 +6,28 @@ export class FriendsRepositoryImpl implements FriendsRepository {
     constructor(private readonly db: PrismaClient) {}
 
     async create(data: CreateFriendDTO): Promise<FriendDTO> {
+        const {addresses, ...friendData} = data;
         const friend = await this.db.friend.create({
-            data
+            data: {
+                ...friendData,
+                addresses: {
+                    create: addresses
+                }
+            }
         })
         return new FriendDTO(friend)
     }
 
     async getAll(): Promise<FriendDTO[]> {
-        const friends = await this.db.friend.findMany();
+        const friends = await this.db.friend.findMany({
+            include: {addresses: true}
+        });
         return friends.map(friend => new FriendDTO(friend))
     }
 
     async delete (id: string): Promise<void> {
         await this.db.friend.delete({
-            where: {
-                id
-            }
+            where: {id}
         })
     }
 }
